ad1f0219e244f9ed9385bb245bcc19520dcf7e19
Gyan Parkash <gyan.p@morrisdigital.mobi>
2019-01-22 11:42:26 +0530

    news format changes in news.service

--- src/app/service/news/news.service.ts
+++ src/app/service/news/news.service.ts
@@ -103,7 +103,8 @@ export class NewsService {
       n.content = 'Content ' + i.toString();
       n.author = 'Gyan';
       n.categories = [Math.floor(Math.random() * 10) + 1, Math.floor(Math.random() * 10) + 1];
-      n.createDate = new Date();
+      let date = new Date();
+      n.createDate = `${date.getFullYear}/${(date.getMonth() + 1)/date.getDate()}`;
       n.published = true;
       n.featuredImage = {
         original: 'assets/images/news/default.jpg',
