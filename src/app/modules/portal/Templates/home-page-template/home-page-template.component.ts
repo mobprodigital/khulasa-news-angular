89f7c50ad08f2bf5f275780ed32d11bcfdd131c6
aakash <aakash.goel462@gmail.com>
2019-01-31 11:21:54 +0530

    add khulasa api in portal

--- src/app/modules/portal/Templates/home-page-template/home-page-template.component.ts
+++ src/app/modules/portal/Templates/home-page-template/home-page-template.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NewsService } from 'src/app/service/news/news.service';
 import { NewsModel } from 'src/app/model/news.model';
-
 @Component({
   selector: 'app-home-page-template',
   templateUrl: './home-page-template.component.html',
   styleUrls: ['./home-page-template.component.css']
 })
 export class HomePageTemplateComponent implements OnInit {
-  @Input() news: NewsModel[]=[];
-  constructor() { }
+  @Input() title: string;
+  @Input() count: number;
+  @Input() categoryId: number;
+  public newsList:NewsModel[]=[];
 
+  constructor(private newsService:NewsService  ) {
+    
+  }
+  public getNewsByCategory() {
+    this.newsService.getNews(this.categoryId,this.count).then(newsdata => this.newsList = newsdata).catch(err => alert(err));
+  }
   ngOnInit() {
+    this.getNewsByCategory();
   }
 
 }
