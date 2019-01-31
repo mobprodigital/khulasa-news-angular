ee5089e5910432e4d39ce84a61a8965d88c06063
Gyan Parkash <gyan.p@morrisdigital.mobi>
2019-01-31 14:02:00 +0530

    overflow on side panels

--- src/app/modules/admin/modules/cms/components/footer/footer.component.ts
+++ src/app/modules/admin/modules/cms/components/footer/footer.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  public readonly currentYear: number = new Date().getFullYear();
 
   constructor() { }
 
